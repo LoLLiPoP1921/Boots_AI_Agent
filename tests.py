9f80ddccf42ca12d55ac0905f8fec4ced14f7b23
LoLLiPoP1921 <jer.bog@hotmail.com>
2025-07-26 00:12:57 +0200

    added run_python_file function to funtions

--- tests.py
+++ tests.py
@@ -1,13 +1,16 @@
-from functions.get_files_info import  write_file
+from functions.get_files_info import  run_python_file
 
 def main():
     print("<-------------------------------------------------->")
-    print(f"Result for current directory:\n{write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")}")
+    print(f"Result for current directory:\n{run_python_file("calculator", "main.py")}")
     print("<-------------------------------------------------->")
-    print(f"Result for 'pkg' directory:\n{write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")}")
+    print(f"Result for 'pkg' directory:\n{run_python_file("calculator", "main.py", ["3 + 5"])}")
     print("<-------------------------------------------------->")
-    print(f"Result for '/bin' directory:\n{write_file("calculator", "/tmp/temp.txt", "this should not be allowed")}")
+    print(f"Result for '/bin' directory:\n{run_python_file("calculator", "tests.py")}")
     print("<-------------------------------------------------->")
-
+    print(f"Result for '/bin' directory:\n{run_python_file("calculator", "../main.py")}")
+    print("<-------------------------------------------------->")
+    print(f"Result for '/bin' directory:\n{run_python_file("calculator", "nonexistent.py")}")
+    print("<-------------------------------------------------->")
 
 main()
